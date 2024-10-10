1f700f920ca8b02007ca845fe6c1d4ea6bd7b4a6
joozooclub464 <86102525+joozooclub464@users.noreply.github.com>
2024-10-10 20:23:05 +0900

    몫 구하기

---
+++ Programmers/Javascript/Lv0/P0_division.js
@@ -0,0 +1,14 @@
+#몫 구하기
+function solution(num1, num2) {
+    var answer = 0;
+    answer = Math.floor(num1/num2);
+    return answer;
+}
+
+const solution = Math.floor(num1/num2)
+
+/*
+몫 만을 구하고 싶을 때 사용하는 버림 함수 Math.floor
+java를 생각하고 나누기만 했는데 계속 실패가 떠서 구글링 후 해결함.
+잊지 말고 기억해두기.
+*/
