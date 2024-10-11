683f2614fd39f103cf91da54d48b6ccb0f382c56
joozooclub464 <86102525+joozooclub464@users.noreply.github.com>
2024-10-12 08:33:01 +0900

    숫자 비교하기

---
+++ Programmers/Javascript/Lv0/P0_compareNum.js
@@ -0,0 +1,15 @@
+#숫자 비교하기
+function solution (num1, num2) {
+    return num1 === num2 ? 1 : -1;
+}
+/*
+function solution(num1, num2) {
+    var answer = 0;
+    if(num1==num2) {
+        answer = 1;
+    } else {
+        answer = -1;
+    }
+    return answer;
+}
+*/
