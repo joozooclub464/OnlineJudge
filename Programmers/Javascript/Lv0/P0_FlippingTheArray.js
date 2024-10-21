b7ff8843c8fec20e4539b5458e36ab5f56c78537
joozooclub464 <86102525+joozooclub464@users.noreply.github.com>
2024-10-21 12:50:16 +0900

    배열 뒤집기

--- Programmers/Javascript/Lv0/P0_FlippingTheArray.js
+++ Programmers/Javascript/Lv0/P0_FlippingTheArray.js
@@ -7,3 +7,8 @@ function solution(num_list) {
     }
     return answer;
 }
+/*
+.push() : 배열 끝에 하나 이상의 요소를 밀어넣기 하는 함수.
+처음에는 num_list 배열의 값에서 하나씩 꺼내어 answer배열에 넣을 생각을 했는데
+그냥 역순으로 꺼내서 push 해주면 되는 간단한 문제였다.
+*/
