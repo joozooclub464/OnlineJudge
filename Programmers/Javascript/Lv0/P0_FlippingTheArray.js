4b782c873b3807343ff45cc22cf22345aad8ceeb
joozooclub464 <86102525+joozooclub464@users.noreply.github.com>
2024-10-21 12:47:50 +0900

    배열 뒤집기

---
+++ Programmers/Javascript/Lv0/P0_FlippingTheArray.js
@@ -0,0 +1,9 @@
+#배열 뒤집기
+function solution(num_list) {
+    var answer = [];
+    var j = num_list.length
+    for(var i = 1; i <= j; i++){
+        answer.push(num_list[j-i])
+    }
+    return answer;
+}
