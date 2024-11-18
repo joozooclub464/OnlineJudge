9672434f8abed5ebc2dcfcd157be32982c3ddee8
joozooclub464 <86102525+joozooclub464@users.noreply.github.com>
2024-11-18 23:55:51 +0900

    문자열 반복해서 출력하기

---
+++ Programmers/Javascript/Lv0/P0_repeatedOutputOfString.js
@@ -0,0 +1,24 @@
+#문자열 반복해서 출력하기
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+let answer = '';
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    str = input[0];
+    n = Number(input[1]);
+    /*
+    for(let i=0; i<n; i++) {
+        answer += str;
+    }
+    console.log(answer);
+    */
+    console.log(str.repeat(n));
+});
